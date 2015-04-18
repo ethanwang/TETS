7032026065f6333577e31b575e8e317410cd29e1
ethan.wang <wangkepengwkp@hotmail.com>
2015-04-18 12:58:26 +0800

    first commit

---
+++ TETS/scripts/2015/zzsc.js
@@ -0,0 +1,77 @@
+﻿// JavaScript Document
+$(document).ready(function (e) {
+    /***不需要自动滚动，去掉即可***/
+    time = window.setInterval(function () {
+        $('.og_next').click();
+    }, 5000);
+    /***不需要自动滚动，去掉即可***/
+    linum = $('.mainlist li').length;//图片数量
+    w = linum * '100%';//ul宽度
+    $('.piclist').css('width', w + 'px');//ul宽度
+
+    $('.og_next').click(function () {
+
+        if ($('.swaplist,.mainlist').is(':animated')) {
+            $('.swaplist,.mainlist').stop(true, true);
+        }
+
+        if ($('.mainlist li').length > 1) {//多于4张图片
+            ml = parseInt($('.mainlist').css('left'));//默认图片ul位置
+            sl = parseInt($('.swaplist').css('left'));//交换图片ul位置
+            if (ml <= 0 && ml > w * -1) {//默认图片显示时
+                $('.swaplist').css({ left: '100%' });//交换图片放在显示区域右侧
+                $('.mainlist').animate({ left: ml - '100%' + 'px' }, 'slow');//默认图片滚动				
+                if (ml == (w - '100%') * -1) {//默认图片最后一屏时
+                    $('.swaplist').animate({ left: '0px' }, 'slow');//交换图片滚动
+                    $('.swaplist').html($('.mainlist').html());//复制内容
+                }
+            } else {//交换图片显示时
+                $('.mainlist').css({ left: '100%' })//默认图片放在显示区域右
+                $('.swaplist').animate({ left: sl - '100%' + 'px' }, 'slow');//交换图片滚动
+                if (sl == (w - '100%') * -1) {//交换图片最后一屏时
+                    $('.mainlist').animate({ left: '0px' }, 'slow');//默认图片滚动
+                    $('.swaplist').html($('.mainlist').html());//复制内容
+                }
+            }
+
+        }
+    })
+    $('.og_prev').click(function () {
+
+        if ($('.swaplist,.mainlist').is(':animated')) {
+            $('.swaplist,.mainlist').stop(true, true);
+        }
+
+        if ($('.mainlist li').length > 1) {
+            ml = parseInt($('.mainlist').css('left'));
+            sl = parseInt($('.swaplist').css('left'));
+            if (ml <= 0 && ml > w * -1) {
+                $('.swaplist').css({ left: w * -1 + 'px' });
+                $('.mainlist').animate({ left: ml + '100%' + 'px' }, 'slow');
+                if (ml == 0) {
+                    $('.swaplist').animate({ left: (w - '100%') * -1 + 'px' }, 'slow');
+                    $('.swaplist').html($('.mainlist').html());//复制内容
+                }
+            } else {
+                $('.mainlist').css({ left: (w - '100%') * -1 + 'px' });
+                $('.swaplist').animate({ left: sl + '100%' + 'px' }, 'slow');
+                if (sl == 0) {
+                    $('.mainlist').animate({ left: '0px' }, 'slow');
+                    $('.swaplist').html($('.mainlist').html());//复制内容
+                }
+            }
+        }
+    })
+});
+
+$(document).ready(function () {
+    $('.og_prev,.og_next').hover(function () {
+        $(this).fadeTo('fast', 1);
+    }, function () {
+        $(this).fadeTo('fast', 0.7);
+    })
+
+})
+
+
+
